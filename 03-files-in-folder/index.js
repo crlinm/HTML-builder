b50f3a6021a55c8c273065a0938aee2298e98eaa
Alina M <crlin.m@gmail.com>
2025-01-21 02:44:54 +0500

    feat(task-03): display info about files in a folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const path = require('node:path');
+const fsp = require('node:fs/promises');
+
+const dir = path.join(__dirname, 'secret-folder');
+
+(async () => {
+  const files = await fsp.readdir(dir, { withFileTypes: true });
+
+  for (const file of files) {
+    if (file.isFile()) {
+      const fileStat = await fsp.stat(path.join(dir, file.name));
+      const ext = path.extname(file.name).slice(1);
+      console.log(
+        [
+          file.name.slice(0, file.name.length - ext.length - 1),
+          ext,
+          fileStat.size + 'bytes',
+        ].join(' - '),
+      );
+    }
+  }
+})();
