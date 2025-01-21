182659366c20e5fe722d612f037ebf0ed2f4fd64
Alina M <crlin.m@gmail.com>
2025-01-21 18:19:52 +0500

    feat(task-06): create index.html using template and content of components

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,19 +5,38 @@ const path = require('node:path');
 (async () => {
   const distFolder = path.join(__dirname, 'project-dist');
   await fsp.mkdir(distFolder, { recursive: true });
-  const bundleCss = path.join(distFolder, 'bundle.css');
+  const bundleCss = path.join(distFolder, 'style.css');
   const stylesFolder = path.join(__dirname, 'styles');
   await mergeStyle(stylesFolder, bundleCss);
 
   const assetsFolder = path.join(__dirname, 'assets');
   const distAssetsFolder = path.join(distFolder, 'assets');
   await copyDir(assetsFolder, distAssetsFolder);
+
+  const componentsFolder = path.join(__dirname, 'components');
+  const indexHTML = await createHTML(componentsFolder);
+  await fsp.writeFile(path.join(distFolder, 'index.html'), indexHTML);
 })();
+
+const createHTML = async (componentsFolder) => {
+  let template = await fsp.readFile(
+    path.join(__dirname, 'template.html'),
+    'utf8',
+  );
+  const components = await fsp.readdir(componentsFolder);
+  for (const component of components) {
+    const componentData = await fsp.readFile(
+      path.join(componentsFolder, component),
+      'utf8',
+    );
+    template = template.replace(`{{${component.slice(0, -5)}}}`, componentData);
+  }
+  return template;
+};
 
 const mergeStyle = async (stylesFolder, bundleCss) => {
   const writeStream = fs.createWriteStream(bundleCss);
   const styleFiles = await fsp.readdir(stylesFolder);
-  console.log(styleFiles);
   for (const style of styleFiles) {
     const ext = path.extname(style);
     if (ext === '.css') {
