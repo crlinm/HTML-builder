86701be9b9234b5ca41f62a89f3799fe76fbe801
Alina M <crlin.m@gmail.com>
2025-01-24 11:31:49 +0500

    feat: fix console input - add breakline

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,29 +1,33 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const readline = require('node:readline/promises');
+const os = require('node:os');
+const { stdin, stdout } = require('node:process');
 
 const file = path.join(__dirname, 'file.txt');
 const writeStream = fs.createWriteStream(file);
 
 const line = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+  input: stdin,
+  output: stdout,
 });
 
 line.on('SIGINT', () => {
-  console.log('Bye!');
-  line.close();
+  closeConsole();
 });
 
-(async () => {
-  for (;;) {
-    const ans = await line.question('Hello! Enter you input: ');
-    if (ans === 'exit') {
-      console.log('Bye!');
-      break;
-    }
-    writeStream.write(ans);
+stdout.write('Hello! Enter you input:\n');
+
+line.on('line', (ans) => {
+  if (ans !== 'exit') {
+    writeStream.write(ans + os.EOL);
+  } else {
+    closeConsole();
   }
+});
+
+const closeConsole = () => {
+  console.log('Bye!');
   line.close();
   writeStream.end();
-})();
+};
